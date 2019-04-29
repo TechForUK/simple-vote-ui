c3e2e838388577103e991b28eff9850416e83d27
Augstin Grigorov <augustin.grigorov@qubit.com>
2019-04-29 18:10:53 +0100

    Styling adjustments

--- src/formViews/EuForm.js
+++ src/formViews/EuForm.js
@@ -5,161 +5,164 @@ function TypeOfVoter({ formData, updateData, submitData }) {
   return (
     <form className="Form" onSubmit={submitData}>
       <label htmlFor="firstName">
-        First Name(s):
+        <span>First Name(s):</span>
         <input id="firstName" name="firstName" type="text" value={formData.firstName} onChange={updateData} />
       </label>
       <label htmlFor="surname">
-        Surname:
+        <span>Surname:</span>
         <input id="surname" name="surname" type="text" value={formData.surname} onChange={updateData} />
       </label>
       <label htmlFor="firstLineAddress">
-        First line of address:
+        <span>First line of address:</span>
         <input id="firstLineAddress" name="firstLineAddress" type="text" value={formData.firstLineAddress} onChange={updateData} />
       </label>
       <label htmlFor="secondLineAddress">
-        Second line of address:
+        <span>Second line of address:</span>
         <input id="secondLineAddress" name="secondLineAddress" type="text" value={formData.secondLineAddress} onChange={updateData} />
       </label>
       <label htmlFor="postcode">
-        Postcode:
+        <span>Postcode:</span>
         <input id="postcode" name="postcode" type="text" value={formData.postcode} onChange={updateData} />
       </label>
       <label htmlFor="city">
-        City/Town:
+        <span>City/Town:</span>
         <input id="city" name="city" type="text" value={formData.city} onChange={updateData} />
       </label>
-      <label htmlFor="differentAddress">
-        I live at another address
+      <label htmlFor="differentAddress" className="Label-SameLine">
         <input id="differentAddress" name="differentAddress" type="checkbox" checked={formData.differentAddress} onChange={updateData} />
+        <span>I live at another address</span>
       </label>
       {formData.differentAddress ? (
         <Fragment>
           <label htmlFor="firstLineCurrentAddress">
-            First line of current address:
+            <span>First line of current address:</span>
             <input id="firstLineCurrentAddress" name="firstLineCurrentAddress" type="text" value={formData.firstLineCurrentAddress} onChange={updateData} />
           </label>
           <label htmlFor="secondLineCurrentAddress">
-            Second line of current address:
+            <span>Second line of current address:</span>
             <input id="secondLineCurrentAddress" name="secondLineCurrentAddress" type="text" value={formData.secondLineCurrentAddress} onChange={updateData} />
           </label>
           <label htmlFor="currentPostcode">
-            Postcode of current address:
+            <span>Postcode of current address:</span>
             <input id="currentPostcode" name="currentPostcode" type="text" value={formData.currentPostcode} onChange={updateData} />
           </label>
           <label htmlFor="currentCity">
-            Current City/Town:
+            <span>Current City/Town:</span>
             <input id="currentCity" name="currentCity" type="text" value={formData.currentCity} onChange={updateData} />
           </label>
         </Fragment>
       ) : null}
       <label htmlFor="citizenOf">
-        I am a citizen of:
+        <span>I am a citizen of:</span>
         <input id="citizenOf" name="citizenOf" type="text" value={formData.citizenOf} onChange={updateData} />
       </label>
-      <label htmlFor="registeredAtHome">
-        My name has been entered on a register of electors in my home country
+      <label htmlFor="registeredAtHome" className="Label-SameLine">
         <input id="registeredAtHome" name="registeredAtHome" type="checkbox" checked={formData.registeredAtHome} onChange={updateData} />
+        <span>My name has been entered on a register of electors in my home country</span>
       </label>
       {formData.registeredAtHome ? (
         <Fragment>
           <label htmlFor="homeCountryConstituency">
-            Name of the locality or constituency where you were last registered, as far as you know
+            <span>
+              Name of the locality or constituency where you
+              were last registered, as far as you know
+            </span>
             <input id="homeCountryConstituency" name="homeCountryConstituency" type="text" value={formData.homeCountryConstituency} onChange={updateData} />
           </label>
         </Fragment>
       ) : null}
       <label htmlFor="email">
-        Email:
+        <span>Email:</span>
         <input id="email" name="email" type="text" value={formData.email} onChange={updateData} />
       </label>
       <label htmlFor="nationality">
-        Nationality (If you have more than one, include all):
+        <span>Nationality (If you have more than one, include all):</span>
         <input id="nationality" name="nationality" type="text" value={formData.nationality} onChange={updateData} />
       </label>
-      <label htmlFor="movedHouse">
-        Have you moved house in the last 12 months?
+      <label htmlFor="movedHouse" className="Label-SameLine">
         <input id="movedHouse" name="movedHouse" type="checkbox" checked={formData.movedHouse} onChange={updateData} />
+        <span>Have you moved house in the last 12 months?</span>
       </label>
       {formData.movedHouse ? (
         <Fragment>
           <label htmlFor="oldAddressFirstLineAddress">
-            First line of address:
+            <span>First line of address:</span>
             <input id="oldAddressFirstLineAddress" name="oldAddressFirstLineAddress" type="text" value={formData.oldAddressFirstLineAddress} onChange={updateData} />
           </label>
           <label htmlFor="oldAddressSecondLineAddress">
-            Second line of address:
+            <span>Second line of address:</span>
             <input id="oldAddressSecondLineAddress" name="oldAddressSecondLineAddress" type="text" value={formData.oldAddressSecondLineAddress} onChange={updateData} />
           </label>
           <label htmlFor="oldAddressPostcode">
-            Postcode:
+            <span>Postcode:</span>
             <input id="oldAddressPostcode" name="oldAddressPostcode" type="text" value={formData.oldAddressPostcode} onChange={updateData} />
           </label>
           <label htmlFor="oldAddressCity">
-            City/Town:
+            <span>City/Town:</span>
             <input id="oldAddressCity" name="oldAddressCity" type="text" value={formData.oldAddressCity} onChange={updateData} />
           </label>
-          <label htmlFor="registeredAsOverseasVoter">
-            I was registered in the UK as an overseas voter
+          <label htmlFor="registeredAsOverseasVoter" className="Label-SameLine">
             <input id="registeredAsOverseasVoter" name="registeredAsOverseasVoter" type="checkbox" checked={formData.registeredAsOverseasVoter} onChange={updateData} />
+            <span>I was registered in the UK as an overseas voter</span>
           </label>
         </Fragment>
       ) : null}
       <label htmlFor="dateOfBirth">
-        Date of birth:
+        <span>Date of birth:</span>
         <input id="dateOfBirth" name="dateOfBirth" type="date" value={formData.dateOfBirth} onChange={updateData} />
       </label>
       <label htmlFor="nin">
-        National insurance number:
+        <span>National insurance number:</span>
         <input id="nin" name="nin" type="text" value={formData.nin} onChange={updateData} />
       </label>
-      <label htmlFor="changedName">
-        I have changed my name
+      <label htmlFor="changedName" className="Label-SameLine">
         <input id="changedName" name="changedName" type="checkbox" checked={formData.changedName} onChange={updateData} />
+        <span>I have changed my name</span>
       </label>
       {formData.changedName ? (
         <Fragment>
           <label htmlFor="previousName">
-            Most recent previous name:
+            <span>Most recent previous name:</span>
             <input id="previousName" name="previousName" type="text" value={formData.previousName} onChange={updateData} />
           </label>
           <label htmlFor="dateOfNameChange">
-            Date of birth:
+            <span>Date of birth:</span>
             <input id="dateOfNameChange" name="dateOfNameChange" type="date" value={formData.dateOfNameChange} onChange={updateData} />
           </label>
         </Fragment>
       ) : null}
-      <label htmlFor="postalVote">
-        I would like to vote by postal vote
+      <label htmlFor="postalVote" className="Label-SameLine">
         <input id="postalVote" name="postalVote" type="checkbox" checked={formData.postalVote} onChange={updateData} />
+        <span>I would like to vote by postal vote</span>
       </label>
       {formData.postalVote ? (
         <Fragment>
-          <label htmlFor="indefintiely">
-            Until further notice
+          <label htmlFor="indefintiely" className="Label-SameLine">
             <input id="indefintiely" name="postalVoteOption" type="radio" value="indefintiely" checked={formData.postalVoteOption === 'indefintiely'} onChange={updateData} />
+            <span>Until further notice</span>
           </label>
-          <label htmlFor="specificElection">
-            For elections to be held on a specific date
+          <label htmlFor="specificElection" className="Label-SameLine">
             <input id="specificElection" name="postalVoteOption" type="radio" value="specificElection" checked={formData.postalVoteOption === 'specificElection'} onChange={updateData} />
+            <span>For elections to be held on a specific date</span>
           </label>
-          <label htmlFor="timePeriod">
-            For a period of time
+          <label htmlFor="timePeriod" className="Label-SameLine">
             <input id="timePeriod" name="postalVoteOption" type="radio" value="timePeriod" checked={formData.postalVoteOption === 'timePeriod'} onChange={updateData} />
+            <span>For a period of time</span>
           </label>
           {formData.postalVoteOption === 'specificElection' ? (
             <label htmlFor="postalVoteElectionDate">
-              Elections on:
+              <span>Elections on:</span>
               <input id="postalVoteElectionDate" name="postalVoteElectionDate" type="date" value={formData.postalVoteElectionDate} onChange={updateData} />
             </label>
           ) : null}
           {formData.postalVoteOption === 'timePeriod' ? (
             <Fragment>
               <label htmlFor="postalVoteFrom">
-                From:
+                <span>From:</span>
                 <input id="postalVoteFrom" name="postalVoteFrom" type="date" value={formData.postalVoteFrom} onChange={updateData} />
               </label>
               <label htmlFor="postalVoteTo">
-                To:
+                <span>To:</span>
                 <input id="postalVoteTo" name="postalVoteTo" type="date" value={formData.postalVoteTo} onChange={updateData} />
               </label>
             </Fragment>
--- src/formViews/GenericForm.js
+++ src/formViews/GenericForm.js
@@ -7,109 +7,109 @@ function TypeOfVoter({ formData, updateData, submitData }) {
     <form className="Form" onSubmit={submitData}>
       <label htmlFor="firstName">
         <span>First Name(s)</span>
-        <input id="firstName" name="firstName" type="text" autocomplete="given-name" value={formData.firstName} onChange={updateData} />
+        <input id="firstName" name="firstName" type="text" value={formData.firstName} onChange={updateData} />
       </label>
       <label htmlFor="surname">
         <span>Surname</span>
-        <input id="surname" name="surname" type="text"  autocomplete="family-name" value={formData.surname} onChange={updateData} />
+        <input id="surname" name="surname" type="text" value={formData.surname} onChange={updateData} />
       </label>
       <label htmlFor="firstLineAddress">
         <span>First line of address</span>
-        <input id="firstLineAddress" name="firstLineAddress" type="text" autocomplete="section-main address-line1" value={formData.firstLineAddress} onChange={updateData} />
+        <input id="firstLineAddress" name="firstLineAddress" type="text" value={formData.firstLineAddress} onChange={updateData} />
       </label>
       <label htmlFor="secondLineAddress">
         <span>Second line of address</span>
-        <input id="secondLineAddress" name="secondLineAddress" type="text" autocomplete="section-main address-line2" value={formData.secondLineAddress} onChange={updateData} />
+        <input id="secondLineAddress" name="secondLineAddress" type="text" value={formData.secondLineAddress} onChange={updateData} />
       </label>
       <label htmlFor="postcode">
         <span>Postcode</span>
-        <input id="postcode" name="postcode" type="text" autocomplete="section-main postal-code" value={formData.postcode} onChange={updateData} />
+        <input id="postcode" name="postcode" type="text" value={formData.postcode} onChange={updateData} />
       </label>
       <label htmlFor="city">
-      <span>City/Town</span>
-        <input id="city" name="city" type="text" autocomplete="" value={formData.city} onChange={updateData} />
+        <span>City/Town</span>
+        <input id="city" name="city" type="text" value={formData.city} onChange={updateData} />
       </label>
-      <label htmlFor="differentAddress">
-      <span>I live at another address</span>
+      <label htmlFor="differentAddress" className="Label-SameLine">
         <input id="differentAddress" name="differentAddress" type="checkbox" checked={formData.differentAddress} onChange={updateData} />
+        <span>I live at another address</span>
       </label>
       <label htmlFor="email">
-      <span>Email</span>
-        <input id="email" name="email" type="text" autocomplete="email" value={formData.email} onChange={updateData} />
+        <span>Email</span>
+        <input id="email" name="email" type="text" value={formData.email} onChange={updateData} />
       </label>
       <label htmlFor="nationality">
-      <span>Nationality (If you have more than one, include all)</span>
-        <input id="nationality" name="nationality" type="text" autocomplete="country-name" value={formData.nationality} onChange={updateData} />
+        <span>Nationality (If you have more than one, include all)</span>
+        <input id="nationality" name="nationality" type="text" value={formData.nationality} onChange={updateData} />
       </label>
-      <label htmlFor="movedHouse">
-      <span>Have you moved house in the last 12 months?</span>
+      <label htmlFor="movedHouse" className="Label-SameLine">
         <input id="movedHouse" name="movedHouse" type="checkbox" checked={formData.movedHouse} onChange={updateData} />
+        <span>Have you moved house in the last 12 months?</span>
       </label>
       {formData.movedHouse ? (
         <Fragment>
           <label htmlFor="oldAddressFirstLineAddress">
-          <span>First line of address</span>
-            <input id="oldAddressFirstLineAddress" name="oldAddressFirstLineAddress" type="text" autocomplete="section-old address-line1" value={formData.oldAddressFirstLineAddress} onChange={updateData} />
+            <span>First line of address</span>
+            <input id="oldAddressFirstLineAddress" name="oldAddressFirstLineAddress" type="text" value={formData.oldAddressFirstLineAddress} onChange={updateData} />
           </label>
           <label htmlFor="oldAddressSecondLineAddress">
-          <span>Second line of address</span>
-            <input id="oldAddressSecondLineAddress" name="oldAddressSecondLineAddress" type="text" autocomplete="section-old address-line2" value={formData.oldAddressSecondLineAddress} onChange={updateData} />
+            <span>Second line of address</span>
+            <input id="oldAddressSecondLineAddress" name="oldAddressSecondLineAddress" type="text" value={formData.oldAddressSecondLineAddress} onChange={updateData} />
           </label>
           <label htmlFor="oldAddressPostcode">
-          <span>Postcode</span>
-            <input id="oldAddressPostcode" name="oldAddressPostcode" type="text" autocomplete="section-old postal-code" value={formData.oldAddressPostcode} onChange={updateData} />
+            <span>Postcode</span>
+            <input id="oldAddressPostcode" name="oldAddressPostcode" type="text" value={formData.oldAddressPostcode} onChange={updateData} />
           </label>
           <label htmlFor="oldAddressCity">
-          <span>City/Town</span>
+            <span>City/Town</span>
             <input id="oldAddressCity" name="oldAddressCity" type="text" value={formData.oldAddressCity} onChange={updateData} />
           </label>
-          <label htmlFor="registeredAsOverseasVoter">
-          <span>I was registered in the UK as an overseas voter</span>
+          <label htmlFor="registeredAsOverseasVoter" className="Label-SameLine">
             <input id="registeredAsOverseasVoter" name="registeredAsOverseasVoter" type="checkbox" checked={formData.registeredAsOverseasVoter} onChange={updateData} />
+            <span>I was registered in the UK as an overseas voter</span>
           </label>
         </Fragment>
       ) : null}
       <label htmlFor="dateOfBirth">
-      <span>Date of birth</span>
-        <input id="dateOfBirth" name="dateOfBirth" type="date"  autocomplete="bday" value={formData.dateOfBirth} onChange={updateData} />
+        <span>Date of birth</span>
+        <input id="dateOfBirth" name="dateOfBirth" type="date" value={formData.dateOfBirth} onChange={updateData} />
       </label>
       <label htmlFor="nin">
-      <span>National insurance number</span>
+        <span>National insurance number</span>
         <input id="nin" name="nin" type="text" value={formData.nin} onChange={updateData} />
       </label>
-      <label htmlFor="changedName">
-      <span>I have changed my name</span>
+      <label htmlFor="changedName" className="Label-SameLine">
         <input id="changedName" name="changedName" type="checkbox" checked={formData.changedName} onChange={updateData} />
+        <span>I have changed my name</span>
       </label>
       {formData.changedName ? (
         <Fragment>
           <label htmlFor="previousName">
-          <span>Most recent previous name</span>
+            <span>Most recent previous name</span>
             <input id="previousName" name="previousName" type="text" value={formData.previousName} onChange={updateData} />
           </label>
           <label htmlFor="dateOfNameChange">
-          <span>Date of birth</span>
+            <span>Date of birth</span>
             <input id="dateOfNameChange" name="dateOfNameChange" type="date" value={formData.dateOfNameChange} onChange={updateData} />
           </label>
         </Fragment>
       ) : null}
-      <label htmlFor="postalVote">
-      <span>I would like to vote by postal vote</span>
+      <label htmlFor="postalVote" className="Label-SameLine">
         <input id="postalVote" name="postalVote" type="checkbox" checked={formData.postalVote} onChange={updateData} />
+        <span>I would like to vote by postal vote</span>
       </label>
       {formData.postalVote ? (
         <Fragment>
-          <label htmlFor="indefintiely">
-          <span>Until further notice</span>
+          <label htmlFor="indefintiely" className="Label-SameLine">
             <input id="indefintiely" name="postalVoteOption" type="radio" value="indefintiely" checked={formData.postalVoteOption === 'indefintiely'} onChange={updateData} />
+            <span>Until further notice</span>
           </label>
-          <label htmlFor="specificElection">
-          <span>For elections to be held on a specific date</span>
+          <label htmlFor="specificElection" className="Label-SameLine">
             <input id="specificElection" name="postalVoteOption" type="radio" value="specificElection" checked={formData.postalVoteOption === 'specificElection'} onChange={updateData} />
+            <span>For elections to be held on a specific date</span>
           </label>
-          <label htmlFor="timePeriod">
-          <span>For a period of time</span>
+          <label htmlFor="timePeriod" className="Label-SameLine">
             <input id="timePeriod" name="postalVoteOption" type="radio" value="timePeriod" checked={formData.postalVoteOption === 'timePeriod'} onChange={updateData} />
+            <span>For a period of time</span>
           </label>
           {formData.postalVoteOption === 'specificElection' ? (
             <label htmlFor="postalVoteElectionDate">
@@ -120,11 +120,11 @@ function TypeOfVoter({ formData, updateData, submitData }) {
           {formData.postalVoteOption === 'timePeriod' ? (
             <Fragment>
               <label htmlFor="postalVoteFrom">
-              <span>From</span>
+                <span>From</span>
                 <input id="postalVoteFrom" name="postalVoteFrom" type="date" value={formData.postalVoteFrom} onChange={updateData} />
               </label>
               <label htmlFor="postalVoteTo">
-              <span>To</span>
+                <span>To</span>
                 <input id="postalVoteTo" name="postalVoteTo" type="date" value={formData.postalVoteTo} onChange={updateData} />
               </label>
             </Fragment>
