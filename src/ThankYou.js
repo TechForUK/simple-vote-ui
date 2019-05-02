9ab9a5e5b0a0b649c6783bd7228c3218fd82002d
Augustin Grigorov <a.a.grigorov@gmail.com>
2019-05-02 19:16:12 +0100

    Add Best for Britain redirect

--- src/ThankYou.js
+++ src/ThankYou.js
@@ -1,19 +1,24 @@
 import React, { Fragment } from 'react';
-import imageFacebook from './img/Facebook.png'
-import imageTwitter from './img/Twitter.png'
-import imageReddit from './img/Reddit.png'
-import imageLinkedIn from './img/LinkedIn.png'
-import imageWordpress from './img/Wordpress.png'
-import imageEmail from './img/Email.png'
-import imageB4bLogo from './img/b4b-new-white-logo.svg'
+import imageFacebook from './img/Facebook.png';
+import imageTwitter from './img/Twitter.png';
+import imageReddit from './img/Reddit.png';
+import imageLinkedIn from './img/LinkedIn.png';
+import imageWordpress from './img/Wordpress.png';
+import imageEmail from './img/Email.png';
+import imageB4bLogo from './img/b4b-new-white-logo.svg';
 
 import './Thankyou.css';
 
 function ThankYou() {
+  setTimeout(() => {
+    window.location = 'https://www.bestforbritain.org/thankyou';
+  }, 5000);
+
   return (
     <Fragment>
       <h1>Thank you!</h1>
       <p>Thank you for submitting your application to register to vote.</p>
+      <p>You will be redirected to Best for Britain in 5 seconds.</p>
       <p>Sharing is caring ... Can you help get someone else enrolled to vote?</p>
       <ul className="share-buttons">
         <li><a href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fsimple.getvoting.org&quote=Get%20Voting%20-%20Register%20to%20vote%20in%20the%20UK%20to%20make%20sure%20you%20can%20vote!%20%23ThisTimeImVoting%20It%20takes%20five%20minutes%20online%20and%20you%20don%E2%80%99t%20need%20a%20printer!%20Make%20sure%20your%20voice%20is%20heard." title="Share on Facebook" target="_blank" rel="noopener noreferrer"><img alt="Share on Facebook" src={imageFacebook} /></a></li>
@@ -24,7 +29,7 @@ function ThankYou() {
         <li><a href="mailto:?subject=Get%20Voting%20-%20Register%20to%20vote%20in%20the%20UK%20to%20make%20sure%20you%20can%20vote!%20%23ThisTimeImVoting%20It%20takes%20five%20minutes%20online%20and%20you%20don%E2%80%99t%20need%20a%20printer!%20Make%20sure%20your%20voice%20is%20heard.&body=:%20https%3A%2F%2Fsimple.getvoting.org" target="_blank" title="Send email" rel="noopener noreferrer"><img alt="Send email" src={imageEmail} /></a></li>
       </ul>
       <p><a href="https://www.bestforbritain.org/" alt="Go To Best for britain"><img className="b4bLogo" src={imageB4bLogo} alt="logo" /></a></p>
-      
+
     </Fragment>
   );
 }
