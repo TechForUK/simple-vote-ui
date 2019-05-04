7f551ffb3e9f05c315cce4aed5f95d05da8a012b
Robert Munro <rob@sentinelweb.co.uk>
2019-05-04 12:53:30 +0100

    faq (not working)

---
+++ src/FAQ.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import close from './img/close.png';
+import './Terms.css';
+
+function FAQ() {
+  return (
+    <div className="TermsContainer">
+      <Link to="/"><img alt="close" src={close} className="CloseButton" /></Link>
+      <h1>Frequently asked questions</h1>
+      <p className="faq_question">It&apos;s very hard to select the DOB year on Android</p>
+      <p className="faq_answer">You can tap the year at the top of the date picker on android to scroll back by year.</p>
+    </div>
+  );
+}
+
+export default FAQ;
